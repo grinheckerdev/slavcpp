5e1dfb1ecd4ccd8e1415d5470ed74c5d04ebfde9
grinheckerdev <148703318+grinheckerdev@users.noreply.github.com>
2023-11-15 18:31:41 +0300

    initial commit

---
+++ СлавянскийC++.h
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+#define число int
+#define взывать cin
+#define вознести return
+#define полк vector
+#define Русь std
+#define внедрить using
+#define хутор namespace
+#define царь_батюшка_главный int main
+#define великое_число long
+#define великое_великое_число long long
+#define воззвать_витязя push_back
+#define размер_казармы size
+#define молвить cout
+#define коль if
+#define кабы else if
+#define инако else
+#define перечислить_крестьян for
